651e30cab5a66a3879052c6fd80c209eec894770
Kley_Muner <kostavlasov834@gmail.com>
2026-06-07 14:06:00 +0300

    some practice

--- Csharp_codes/Program.cs
+++ Csharp_codes/Program.cs
@@ -1,7 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 int my_number = 2;
 string my_string = "Hello";
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 27;
+            string name = "Konstantin";
+
+            Console.WriteLine("Your name is: " + name + " and you are " + age + " years old");
+            Console.WriteLine("Your age is: " + age);
+
+        }
+    }
+}
+
+
+
 
 for (int i = 0; i < my_number; i++)
 {
